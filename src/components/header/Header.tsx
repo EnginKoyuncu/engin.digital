5b5a31b64196742dc13c2edba25e05ffc19115a0
EnginKoyuncu <engin.koyuncu@student.arteveldehs.be>
2022-09-18 00:24:37 +0200

    first push

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,20 @@
+const Header = () => (
+    <nav>
+        <div className="d-flex justify-content-between">
+        <div className="">
+            <div>Logo</div>
+        </div>
+
+        <div >
+            <ul className="d-flex">
+                <li>Home</li>
+                <li>Service</li>
+                <li>About us</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+)
+
+export default Header;
---
+++ src/pages/homepage/homepage.tsx
@@ -0,0 +1,14 @@
+import Header from "../../components/header/Header"
+
+const HomePage = () => {
+    return (
+        <>
+        <div className="bg_main">
+        <Header />
+        <div>This is homepage</div>
+        </div>
+        </>
+    )
+}
+
+export default HomePage;
